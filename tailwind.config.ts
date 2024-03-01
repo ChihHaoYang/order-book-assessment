5e478d7079e698de158264e4c0983418bd2c5359
HectorYang <how30013@hotmail.com>
2024-03-01 20:24:57 +0800

    feat: add tailwind custom classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      textColor: {
+        default: '#F0F4F8',
+        'quote-head': '#8698aa',
+        'quote-buy': '#00b15d',
+        'quote-sell': '#ff5b5a',
+        'last-p-same': '#f0f4f8',
+        'last-p-high': '#00b15d',
+        'last-p-low': '#ff5b5a'
       },
-    },
+      backgroundColor: {
+        book: '#131B29',
+        'last-p-same': 'rgba(134, 152, 170, 0.12)',
+        'last-p-high': 'rgba(16, 186, 104, 0.12)',
+        'last-p-low': 'rgba(255, 90, 90, 0.12)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
