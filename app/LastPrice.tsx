a12b74f0b04593dece0f6a94183b29c36d76f8e7
HectorYang <how30013@hotmail.com>
2024-03-01 20:25:53 +0800

    feat: add pure components for display

---
+++ app/LastPrice.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { formatNumber } from './utils';
+
+type PriceRowProps = {
+  price: number;
+  className: string;
+};
+
+const LastPrice = ({ price, className }: PriceRowProps) => {
+  return (
+    <tr>
+      <td
+        className={`text-default text-lg text-center ${className}`}
+        colSpan={3}
+      >
+        {formatNumber(price, 1)}
+      </td>
+    </tr>
+  );
+};
+
+export default LastPrice;
---
+++ app/PriceRow.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { formatNumber } from './utils';
+
+type PriceRowProps = {
+  price: number;
+  size: number;
+  total: number;
+  type: 'buy' | 'sell';
+};
+
+const PriceRow = ({ price, size, total, type }: PriceRowProps) => {
+  const quoteClass = type === 'buy' ? 'text-quote-buy' : 'text-quote-sell';
+  return (
+    <tr className='my-2'>
+      <td className={`${quoteClass}`}>{formatNumber(price, 1)}</td>
+      <td className='text-default text-right'>{formatNumber(size, 0)}</td>
+      <td className='text-default text-right'>{formatNumber(total, 0)}</td>
+    </tr>
+  );
+};
+
+export default PriceRow;
