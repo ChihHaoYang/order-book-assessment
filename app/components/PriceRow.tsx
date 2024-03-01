cd5194b0df31253b162d0df6641befa74d69af69
HectorYang <how30013@hotmail.com>
2024-03-02 00:45:30 +0800

    feat: add size flash

--- app/components/PriceRow.tsx
+++ app/components/PriceRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Roboto_Mono } from 'next/font/google';
 import { formatNumber } from '../utils';
 
@@ -14,7 +14,27 @@ const robotoMono = Roboto_Mono({ subsets: ['latin'] });
 
 const PriceRow = React.memo(
   ({ price, size, total, type, flash }: PriceRowProps) => {
+    const [flashCellClass, setFlashCellClass] = useState<string>('');
+    const prevSize = useRef<number>(size);
     const quoteClass = type === 'buy' ? 'text-quote-buy' : 'text-quote-sell';
+
+    useEffect(() => {
+      if (prevSize.current) {
+        if (prevSize.current > size) {
+          setFlashCellClass('animate-green-flash');
+          prevSize.current = size;
+        } else if (prevSize.current < size) {
+          setFlashCellClass('animate-red-flash');
+          prevSize.current = size;
+        }
+        setTimeout(() => {
+          setFlashCellClass('');
+        }, 1000);
+      } else {
+        setFlashCellClass('');
+        prevSize.current = size;
+      }
+    }, [size]);
 
     const trClass = flash
       ? type === 'buy'
@@ -27,7 +47,9 @@ const PriceRow = React.memo(
         className={`my-2 pointer  hover:bg-book-hover ${robotoMono.className} ${trClass}`}
       >
         <td className={`${quoteClass}`}>{formatNumber(price, 1)}</td>
-        <td className='text-default text-right'>{formatNumber(size, 0)}</td>
+        <td className={`text-default text-right ${flashCellClass}`}>
+          {formatNumber(size, 0)}
+        </td>
         <td colSpan={3} className='text-default text-right'>
           {formatNumber(total, 0)}
         </td>
