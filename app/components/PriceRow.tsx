53e59b562bc65ab10bfc9ee05605ed6d5f34c400
HectorYang <how30013@hotmail.com>
2024-03-05 12:12:39 +0800

    fix: fix building issue display-name

--- app/components/PriceRow.tsx
+++ app/components/PriceRow.tsx
@@ -13,56 +13,61 @@ type PriceRowProps = {
 
 const robotoMono = Roboto_Mono({ subsets: ['latin'] });
 
-const PriceRow = React.memo(
-  ({ price, size, total, type, flash, percentage }: PriceRowProps) => {
-    const [flashCellClass, setFlashCellClass] = useState<string>('');
-    const prevSize = useRef<number>(size);
-    const quoteClass = type === 'buy' ? 'text-quote-buy' : 'text-quote-sell';
+const PriceRow = ({
+  price,
+  size,
+  total,
+  type,
+  flash,
+  percentage
+}: PriceRowProps) => {
+  const [flashCellClass, setFlashCellClass] = useState<string>('');
+  const prevSize = useRef<number>(size);
+  const quoteClass = type === 'buy' ? 'text-quote-buy' : 'text-quote-sell';
 
-    useEffect(() => {
-      if (prevSize.current) {
-        if (prevSize.current > size) {
-          setFlashCellClass('animate-green-flash');
-          prevSize.current = size;
-        } else if (prevSize.current < size) {
-          setFlashCellClass('animate-red-flash');
-          prevSize.current = size;
-        }
-        setTimeout(() => {
-          setFlashCellClass('');
-        }, 1000);
-      } else {
-        setFlashCellClass('');
+  useEffect(() => {
+    if (prevSize.current) {
+      if (prevSize.current > size) {
+        setFlashCellClass('animate-green-flash');
+        prevSize.current = size;
+      } else if (prevSize.current < size) {
+        setFlashCellClass('animate-red-flash');
         prevSize.current = size;
       }
-    }, [size]);
+      setTimeout(() => {
+        setFlashCellClass('');
+      }, 1000);
+    } else {
+      setFlashCellClass('');
+      prevSize.current = size;
+    }
+  }, [size]);
 
-    const trClass = flash
-      ? type === 'buy'
-        ? 'animate-green-flash'
-        : 'animate-red-flash'
-      : '';
+  const trClass = flash
+    ? type === 'buy'
+      ? 'animate-green-flash'
+      : 'animate-red-flash'
+    : '';
 
-    const barClass = type === 'buy' ? 'bg-bar-buy' : 'bg-bar-sell';
+  const barClass = type === 'buy' ? 'bg-bar-buy' : 'bg-bar-sell';
 
-    return (
-      <tr
-        className={`relative my-2 pointer  hover:bg-book-hover ${robotoMono.className} ${trClass}`}
-      >
-        <td className={`${quoteClass}`}>{formatNumber(price, 1)}</td>
-        <td className={`text-default text-right ${flashCellClass}`}>
-          {formatNumber(size, 0)}
-        </td>
-        <td colSpan={3} className='text-default text-right'>
-          {formatNumber(total, 0)}
-        </td>
-        <td
-          className={`absolute h-full right-0 ${barClass}`}
-          style={{ width: `${percentage}%` }}
-        />
-      </tr>
-    );
-  }
-);
+  return (
+    <tr
+      className={`relative my-2 pointer  hover:bg-book-hover ${robotoMono.className} ${trClass}`}
+    >
+      <td className={`${quoteClass}`}>{formatNumber(price, 1)}</td>
+      <td className={`text-default text-right ${flashCellClass}`}>
+        {formatNumber(size, 0)}
+      </td>
+      <td colSpan={3} className='text-default text-right'>
+        {formatNumber(total, 0)}
+      </td>
+      <td
+        className={`absolute h-full right-0 ${barClass}`}
+        style={{ width: `${percentage}%` }}
+      />
+    </tr>
+  );
+};
 
-export default PriceRow;
+export default React.memo(PriceRow);
